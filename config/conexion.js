c635a58b1da4cd6f70039b4588974b9ae2548714
eugeniojal <eugeniojal1@gmail.com>
2022-04-22 14:46:40 -0400

    eugenio

--- config/conexion.js
+++ config/conexion.js
@@ -1,50 +1,35 @@
-const mysql = require('mysql');
-
-const conexion = mysql.createPool({
-    connectionLimit: 50,
-    host: '127.0.0.1',
-    user: 'tuplanet_planetadulce',
-    password: 'espiderman18$$',
-    database: 'tuplanet_planetadulce',
-    debug: false
-});
-// const conexion =  mysql.createConnection({
-//     host     : '127.0.0.1',
-//     user     : 'tuplanet_planetadulce',
-//     password : 'espiderman18$$',
-//     database : 'tuplanet_planetadulce',
-// });
-// conexion.connect( (err) => {
-
-//     if (err) {
-//         console.log('error eugeno :'+err);
-//         return err;
-//     }
-
-//     console.log('Conexion establecida');
-// })
-
-module.exports = conexion;
-
-
 // const mysql = require('mysql');
 
-// const conexion =  mysql.createConnection({
-//     host: 'localhost',
-//     user:'root',
-//     password:'',
-//     database:'planetadulce',
-
+// const conexion = mysql.createPool({
+//     connectionLimit: 50,
+//     host: '127.0.0.1',
+//     user: 'tuplanet_planetadulce',
+//     password: 'espiderman18$$',
+//     database: 'tuplanet_planetadulce',
+//     debug: false
 // });
 
-// conexion.connect( (err) => {
-
-//     if (err) {
-//         console.log(err);
-//         return err;
-//     }
-
-//     console.log('Conexion establecida');
-// })
-
 // module.exports = conexion;
+
+
+const mysql = require('mysql');
+
+const conexion =  mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database:'planetadulce',
+
+});
+
+conexion.connect( (err) => {
+
+    if (err) {
+        console.log(err);
+        return err;
+    }
+
+    console.log('Conexion establecida');
+})
+
+module.exports = conexion;
